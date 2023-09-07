caff8ff57454847ecbe4c316c415da80bcba8664
AresBC <62770044+AresBC@users.noreply.github.com>
2023-09-07 16:19:22 +0200

    add jq.schedule libs

--- src/view/sites/schedule.php
+++ src/view/sites/schedule.php
@@ -1,4 +1,8 @@
 <?php $TEMPLATE = 'main'; ?>
 
+<script defer src="/public/assets/js/jQuery.js"></script>
+<script defer src="/public/assets/js/jQuery-ui.js"></script>
+<script defer src="/public/assets/js/bootstrap.js"></script>
+<script defer src="/public/assets/js/schedule.js"></script>
 
 <h1>Schedule</h1>
